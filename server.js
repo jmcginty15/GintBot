6474e2f86dca18c6eea8ccdf1ef886cbafd83f29
Jason McGinty <jason_mcginty@yahoo.com>
2021-12-09 20:02:05 -0600

    adding http port

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 const packageInfo = require('./package.json');
+const PORT = process.env.PORT || 3001;
 require('./bot');
 
 app.get('/', (req, res) => {
     res.json({ version: packageInfo.version });
 });
 
-app.listen(3001, () => {
-    console.log(`Web server started on port ${3001}`);
+app.listen(PORT, () => {
+    console.log(`Web server started on port ${PORT}`);
 });
