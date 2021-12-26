153c103f836da0771034087554b3cc458a5bade0
Jason McGinty <jason_mcginty@yahoo.com>
2021-12-26 13:04:51 -0600

    down for maintenance

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const packageInfo = require('./package.json');
 const PORT = process.env.PORT || 3001;
-const { bot, API_TOKEN } = require('./bot');
+// const { bot, API_TOKEN } = require('./bot');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -11,10 +11,10 @@ app.get('/', (req, res) => {
     res.json({ version: packageInfo.version });
 });
 
-app.post(`/${API_TOKEN}`, (req, res) => {
-    bot.processUpdate(req.body);
-    res.sendStatus(200);
-});
+// app.post(`/${API_TOKEN}`, (req, res) => {
+//     bot.processUpdate(req.body);
+//     res.sendStatus(200);
+// });
 
 app.listen(PORT, () => {
     console.log(`Web server started on port ${PORT}`);
